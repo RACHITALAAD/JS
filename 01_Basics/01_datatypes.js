91e39279a51ef4e0b1cf636d1ac010d8bc64798e
RACHITALAAD <152146800+RACHITALAAD@users.noreply.github.com>
2024-07-24 06:29:33 +0000

    commit done

---
+++ 01_Basics/01_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all Js code as newer version
+
+// alert("hello"); // we are using nodejs ; not browser
+
+console.log("hello"); // code readability should be high
+
+console.log(3+3) // readable code  //good practice
+
+let name = "rachita" //string
+let age = 20 //number
+let isLoggedIn = false
+let state;  // variable declare but not define
+let state1 = null; // empty
+
+
+// number => 2 to power 53  // range
+// bigInt => big number 
+// string => " "
+// boolean => ha ya na // true/false
+// null => standalone value  // it is a representation of empty value
+// undefined => declare but not define
+// symbol => unique
+
+
+// Non primitive data types
+// object
+
+console.log(typeof "rachita");
+console.log(typeof age);
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
+
---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 144553
+let accountEmail = "rachilaad@gmail.com"
+var accountPassword = "12345"
+accountCity = "Khargone"
+
+// accountId = 225452
+
+console.log(accountId);

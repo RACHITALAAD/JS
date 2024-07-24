c1a37647438ab1a4e37171e5ed38843e83d6eec6
RACHITALAAD <152146800+RACHITALAAD@users.noreply.github.com>
2024-07-24 13:32:07 +0000

    commit done

--- 01_Basics/01_conversion_operations.js
+++ 01_Basics/01_conversion_operations.js
@@ -50,6 +50,46 @@ let string = String(someNumber);
 console.log(string);
 console.log(typeof string)
 
+// **Operations**
+
+let value = 3;
+let negvalue = -value;
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello";
+let str2 = " rachita";
+let str3 = str1+str2;
+
+console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1" + 2 + 2);
+// console.log(1+2+"2");
+
+console.log(true);
+// console.log(+true);
+// console.log(true+);
+
+console.log(+"");
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 150;
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter)
+
 
 
 

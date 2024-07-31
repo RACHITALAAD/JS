3f40d08bee17d826fa9195cf6a144f81cc014596
RACHITALAAD <152146800+RACHITALAAD@users.noreply.github.com>
2024-07-31 15:16:15 +0000

    commit done

--- 03_Basics/03_Scope.js
+++ 03_Basics/03_Scope.js
@@ -12,15 +12,62 @@ if(true){
     let a = 10  // block scoped
     const b = 20
     var c = 30
-    console.log(a);
-    console.log(b);
+    // console.log(a);
+    // console.log(b);
 }
 
 // console.log(a);
 // console.log(b);
 // console.log(c);
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+// }
+
+// nested scope
+
+function one(){
+    const username = "rachita";
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+
+    // console.log(website);
+
+    // two()
 }
+
+one();
+
+// closures 
+
+if(true){
+    const username = "rachita"
+    if(username === "rachita"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+//     console.log(username);
+ }
+
+// console.log(username);
+
+// ***** IMP *****
+
+console.log(addOne(5));  // it is execute 
+function addOne(num){  // function 
+    return num + 1;
+}
+
+// addOne(5)
+
+// addTwo(5);  // error
+const addTwo = function(num){  // expression   // a variable hold a function //hosting 
+    return num+2;
+}
+
+// addTwo(5);
 
